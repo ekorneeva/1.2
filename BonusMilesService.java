810878afb3c8bcb99705cee596e32a7f6f52cba2
ekorneeva <43140015+ekorneeva@users.noreply.github.com>
2022-01-09 13:02:33 +0300

    Delete BonusMilesService.java

--- BonusMilesService.java
+++
@@ -1,9 +0,0 @@
-public class BonusMilesService {
-    public int calculate (int cost) {
-        int bonusMiles = 1;
-        int rubles = 20;
-        int totalBonus = cost * bonusMiles / rubles;
-
-        return totalBonus;
-    }
-}
