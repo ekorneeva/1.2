cf0dbaf4351b1dafaa0a17e4743f6c953ac60355
ekorneeva <43140015+ekorneeva@users.noreply.github.com>
2022-01-26 16:56:44 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int account = 100;
-        int sum = 1100;
-        int bonus;
-        int percent = 1;
-        if (sum <= 1000) {
-            bonus = 0;
-        } else {
-            bonus = sum * percent / 100;
-        }
-        int total = account + sum + bonus;
-        System.out.println(total);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
